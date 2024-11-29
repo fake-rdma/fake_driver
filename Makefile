a9ab05c24147dd56b4cea73f1e0467baf509805c
chenmiao <3026066798@qq.com>
2024-11-30 05:36:52 +0800

    [frdma feat]: init the first code, gen module

--- Makefile
+++ Makefile
@@ -1,10 +1,27 @@
-DEPS_LINUX := deps/linux-6.11.8
+KERNEL_VERSION = $(shell uname -r)
+
+REMOTE_KERNEL := $(shell pwd)/deps/linux-6.11.8/
+LOCAL_KERNEL ?= /lib/modules/${KERNEL_VERSION}/build  
+
+DEPS_LINUX ?= $(REMOTE_KERNEL)
+IS_LOCAL ?= $(shell ./prebuild.sh)
+IS_OK := 1
+
+# prepare_bash:
+	
+ifeq ($(IS_OK),$(IS_LOCAL))
+	DEPS_LINUX := $(LOCAL_KERNEL)
+else
+	DEPS_LINUX := $(REMOTE_KERNEL)
+endif
 
 all:
-	@echo "Run Prebuild Script"
+	@echo "Using kernel directory: ${DEPS_LINUX}"
+	@echo "Using DEPS_LINUX=${DEPS_LINUX}"
+	@bear -- $(MAKE) -C impl DEPS_LINUX=$(DEPS_LINUX)
 
-	@bash prebuild.sh
-	
-	@echo "Run Successful"
+.PHONY: clean
 
-	$(MAKE) -C impl
+clean:
+	${MAKE} -C impl DEPS_LINUX=$(DEPS_LINUX) clean
+	sudo ${MAKE} -C $(DEPS_LINUX) clean
--- prebuild.sh
+++ prebuild.sh
@@ -1,18 +1,43 @@
 #!/bin/bash
 
-pushd deps
+KERNEL_VERSION=$(uname -r)
 
-if [ -d "linux-6.11.8" ]; then
-  echo "Directory linux-6.11.8 already exists."
-else
-  echo "Extracting linux-6.11.8.tar.gz..."
-  if [ -f "linux-6.11.8.tar.gz" ]; then
-    tar zxvf linux-6.11.8.tar.gz > /dev/null
-  else
-    echo "Error: linux-6.11.8.tar.gz not found."
-    exit 1
+if [ -d "/lib/modules/${KERNEL_VERSION}/build" ]; then
+  LOCAL_KERNEL="/lib/modules/${KERNEL_VERSION}/build"
+  if [ ! -f "${LOCAL_KERNEL}/.config" ]; then
+    sudo cp /boot/config-6.11.8-300.fc41.aarch64 ${LOCAL_KERNEL}/.config
   fi
-  echo "Extraction complete."
-fi
+  echo 1
+else
+  KDIR="$(pwd)/deps/linux-6.11.8"
 
-popd
+  pushd deps
+
+  if [ -d "linux-6.11.8" ]; then
+    echo "Directory linux-6.11.8 already exists."
+  else
+    echo "Extracting linux-6.11.8.tar.gz..."
+    if [ -f "linux-6.11.8.tar.gz" ]; then
+      tar zxvf linux-6.11.8.tar.gz > /dev/null
+    else
+      echo "Error: linux-6.11.8.tar.gz not found."
+      exit 1
+    fi
+    echo "Extraction complete."
+  fi
+
+  pushd linux-6.11.8
+
+  echo $(pwd)
+  if [ ! -f ".config" ]; then
+    echo "start to build kernel"
+    sudo cp ../../.config .
+  else
+    echo "already have RDMA config"
+  fi
+
+  popd
+  popd
+
+  echo 0
+fi
