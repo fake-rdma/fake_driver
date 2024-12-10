598ab2713ad4f5a12564ae6dc97976951107950e
chenmiao <3026066798@qq.com>
2024-12-11 06:08:31 +0800

    [frdma feat]: clean the tree and completion code

--- impl/fake_driver.c
+++ impl/fake_driver.c
@@ -1,3 +1,4 @@
+#include "linux/dev_printk.h"
 #include "linux/init.h"
 #include "linux/kern_levels.h"
 #include "linux/printk.h"
@@ -12,140 +13,107 @@
 MODULE_AUTHOR( "Chen Miao <chenmiao.ku@gmail.com>" );
 MODULE_LICENSE( "Dual BSD/GPL" );
 
-static struct rdma_hw_stats* frdma_alloc_hw_port_stats( struct ib_device* device,
-                                                        u32               port_num ) {
-  return NULL;
-}
-
-static int frdma_get_hw_stats( struct ib_device*     ibdev,
-                               struct rdma_hw_stats* stats,
-                               u32                   port,
-                               int                   index ) {
-  return 0;
-}
-
-static int frdma_get_port_immutable( struct ib_device*         dev,
-                                     u32                       port,
-                                     struct ib_port_immutable* port_immutable ) {
-  // port_immutable->gid_tbl_len    = 1;
-  // port_immutable->core_cap_flags = RDMA_CORE_PORT_IWARP;
-  return 0;
-}
-
-static int frdma_map_mr_sg( struct ib_mr*       ibmr,
-                            struct scatterlist* sg,
-                            int                 sg_nents,
-                            unsigned int*       sg_offset ) {
-  return 0;
-}
-
-static int frdma_mmap( struct ib_ucontext* ctx, struct vm_area_struct* vma ) {
-  return 0;
-}
-
-static void frdma_mmap_free( struct rdma_user_mmap_entry* rdma_entry ) {}
-
-static int frdma_modify_qp( struct ib_qp*      ibqp,
-                            struct ib_qp_attr* attr,
-                            int                mask,
-                            struct ib_udata*   data ) {
-  return 0;
-}
-
-static int frdma_query_device( struct ib_device*      dev,
-                               struct ib_device_attr* attr,
-                               struct ib_udata*       data ) {
-  memset( attr, 0, sizeof( *attr ) );
-  return 0;
-}
-
 static int frdma_query_gid( struct ib_device* dev,
                             u32               port,
                             int               idx,
                             union ib_gid*     gid ) {
+
   return 0;
 }
 
-static int frdma_query_port( struct ib_device*    dev,
+static inline struct frdma_dev* to_fdev( struct ib_device* ibdev ) {
+  return container_of( ibdev, struct frdma_dev, ibdev );
+}
+
+static int frdma_get_port_immutable( struct ib_device*         ibdev,
+                                     u32                       port,
+                                     struct ib_port_immutable* port_immutable ) {
+  struct ib_port_attr attr = {};
+  int                 err;
+
+  if ( port != 1 ) {
+    err = -EINVAL;
+    dev_err( &ibdev->dev, "bad port_num = %d\n", port );
+    goto err_out;
+  }
+
+  err = ib_query_port( ibdev, port, &attr );
+  if ( err ) goto err_out;
+
+  port_immutable->core_cap_flags = RDMA_CORE_PORT_IBA_ROCE_UDP_ENCAP;
+  port_immutable->pkey_tbl_len   = attr.pkey_tbl_len;
+  port_immutable->gid_tbl_len    = attr.gid_tbl_len;
+  port_immutable->max_mad_size   = IB_MGMT_MAD_SIZE;
+
+  return 0;
+
+err_out:
+  return err;
+}
+
+static int frdma_query_device( struct ib_device*      ibdev,
+                               struct ib_device_attr* attr,
+                               struct ib_udata*       data ) {
+  struct frdma_dev* dev = to_fdev( ibdev );
+  int               err;
+
+  if ( data->inlen || data->outlen ) {
+    dev_err( &ibdev->dev, "malformed udata\n" );
+    err = -EINVAL;
+    goto err_out;
+  }
+
+  memset( attr, 0, sizeof( *attr ) );
+  memcpy( attr, &dev->attrs, sizeof( *attr ) );
+
+  return 0;
+
+err_out:
+  dev_err( &ibdev->dev, "returned err = %d\n", err );
+  return err;
+}
+
+static int frdma_query_port( struct ib_device*    ibdev,
                              u32                  port,
                              struct ib_port_attr* attr ) {
+  struct frdma_dev* dev = to_fdev( ibdev );
+  int               err;
+
+  if ( port != 1 ) {
+    err = -EINVAL;
+    dev_err( &ibdev->dev, "bad port_num = %d\n", port );
+    goto err_out;
+  }
+
+  memcpy( attr, &dev->port.attr, sizeof( *attr ) );
+
+  attr->state      = IB_PORT_ACTIVE;
+  attr->phys_state = IB_PORT_PHYS_STATE_LINK_UP;
   return 0;
+
+err_out:
+  dev_err( &ibdev->dev, "returned err = %d\n", err );
+  return err;
 }
 
-static int frdma_query_qp( struct ib_qp*           ibqp,
-                           struct ib_qp_attr*      attr,
-                           int                     mask,
-                           struct ib_qp_init_attr* init_attr ) {
+static int frdma_query_pkey( struct ib_device* ibdev,
+                             u32               port_num,
+                             u16               index,
+                             u16*              pkey ) {
+  struct frdma_dev* dev = to_fdev( ibdev );
+  int               err;
+
+  if ( index != 0 ) {
+    err = -EINVAL;
+    dev_err( &ibdev->dev, "bad pkey index = %d\n", index );
+    goto err_out;
+  }
+  *pkey = IB_DEFAULT_PKEY_FULL;
   return 0;
-}
 
-static int frdma_req_notify_cq( struct ib_cq*           ibcq,
-                                enum ib_cq_notify_flags flags ) {
-  return 0;
-}
-
-static struct ib_mr* frdma_reg_user_mr( struct ib_pd*    ibpd,
-                                        u64              start,
-                                        u64              len,
-                                        u64              virt,
-                                        int              access,
-                                        struct ib_udata* udata ) {
-  return NULL;
-}
-
-static int frdma_alloc_pd( struct ib_pd* ibpd, struct ib_udata* udata ) {
-  return 0;
-}
-
-static int frdma_dealloc_pd( struct ib_pd* ibpd, struct ib_udata* udata ) {
-  return 0;
-}
-
-static int frdma_create_qp( struct ib_qp*           ibqp,
-                            struct ib_qp_init_attr* attrs,
-                            struct ib_udata*        udata ) {
-  return 0;
-}
-
-static int frdma_destroy_cq( struct ib_cq* ibcq, struct ib_udata* udata ) {
-  return 0;
-}
-
-static int frdma_destroy_qp( struct ib_qp* ibqp, struct ib_udata* udata ) {
-  return 0;
-}
-
-static int frdma_post_send( struct ib_qp*             ibqp,
-                            const struct ib_send_wr*  send_wr,
-                            const struct ib_send_wr** bad_send_wr ) {
-  return 0;
-}
-
-static int frdma_post_recv( struct ib_qp*             ibqp,
-                            const struct ib_recv_wr*  recv_wr,
-                            const struct ib_recv_wr** bad_recv_wr ) {
-  return 0;
-}
-
-static int frdma_create_cq( struct ib_cq*                 ibcq,
-                            const struct ib_cq_init_attr* attr,
-                            struct uverbs_attr_bundle*    attrs ) {
-  return 0;
-}
-
-static int frdma_poll_cq( struct ib_cq* ibcq,
-                          int           num_entries,
-                          struct ib_wc* wc ) {
-  return 0;
-}
-
-
-static struct ib_mr* frdma_get_dma_mr( struct ib_pd* ibpd, int acc ) {
-  return NULL;
-}
-
-static int frdma_dereg_mr( struct ib_mr* ibmr, struct ib_udata* udata ) {
-  return 0;
+err_out:
+  dev_err( &ibdev->dev, "returned err = %d\n", err );
+  return err;
 }
 
 const struct ib_device_ops frdma_device_ops = {
@@ -155,24 +123,65 @@ const struct ib_device_ops frdma_device_ops = {
 
   .query_device       = frdma_query_device,
   .query_port         = frdma_query_port,
-  .alloc_pd           = frdma_alloc_pd,
-  .dealloc_pd         = frdma_dealloc_pd,
-  .create_qp          = frdma_create_qp,
-  .modify_qp          = frdma_modify_qp,
-  .destroy_qp         = frdma_destroy_qp,
-  .post_send          = frdma_post_send,
-  .post_recv          = frdma_post_recv,
-  .create_cq          = frdma_create_cq,
-  .destroy_cq         = frdma_destroy_cq,
-  .poll_cq            = frdma_poll_cq,
-  .req_notify_cq      = frdma_req_notify_cq,
-  .get_dma_mr         = frdma_get_dma_mr,
-  .reg_user_mr        = frdma_reg_user_mr,
-  .dereg_mr           = frdma_dereg_mr,
+  .query_pkey         = frdma_query_pkey,
   .get_port_immutable = frdma_get_port_immutable,
 };
 
 static struct frdma_dev* dev;
+
+static void frdma_attr_init( struct frdma_dev* dev ) {
+  dev->attrs.vendor_id                  = FRDMA_VENDOR_ID;
+  dev->attrs.max_mr_size                = FRDMA_MAX_MR_SIZE;
+  dev->attrs.page_size_cap              = FRDMA_PAGE_SIZE_CAP;
+  dev->attrs.max_qp                     = FRDMA_MAX_QP;
+  dev->attrs.max_qp_wr                  = FRDMA_MAX_QP_WR;
+  dev->attrs.device_cap_flags           = FRDMA_DEVICE_CAP_FLAGS;
+  dev->attrs.kernel_cap_flags           = IBK_ALLOW_USER_UNREG;
+  dev->attrs.max_send_sge               = FRDMA_MAX_SGE;
+  dev->attrs.max_recv_sge               = FRDMA_MAX_SGE;
+  dev->attrs.max_sge_rd                 = FRDMA_MAX_SGE_RD;
+  dev->attrs.max_cq                     = FRDMA_MAX_CQ;
+  dev->attrs.max_cqe                    = ( 1 << FRDMA_MAX_LOG_CQE ) - 1;
+  dev->attrs.max_mr                     = FRDMA_MAX_MR;
+  dev->attrs.max_mw                     = FRDMA_MAX_MW;
+  dev->attrs.max_pd                     = FRDMA_MAX_PD;
+  dev->attrs.max_qp_rd_atom             = FRDMA_MAX_QP_RD_ATOM;
+  dev->attrs.max_res_rd_atom            = FRDMA_MAX_RES_RD_ATOM;
+  dev->attrs.max_qp_init_rd_atom        = FRDMA_MAX_QP_INIT_RD_ATOM;
+  dev->attrs.atomic_cap                 = IB_ATOMIC_HCA;
+  dev->attrs.max_mcast_grp              = FRDMA_MAX_MCAST_GRP;
+  dev->attrs.max_mcast_qp_attach        = FRDMA_MAX_MCAST_QP_ATTACH;
+  dev->attrs.max_total_mcast_qp_attach  = FRDMA_MAX_TOT_MCAST_QP_ATTACH;
+  dev->attrs.max_ah                     = FRDMA_MAX_AH;
+  dev->attrs.max_srq                    = FRDMA_MAX_SRQ;
+  dev->attrs.max_srq_wr                 = FRDMA_MAX_SRQ_WR;
+  dev->attrs.max_srq_sge                = FRDMA_MAX_SRQ_SGE;
+  dev->attrs.max_fast_reg_page_list_len = FRDMA_MAX_FMR_PAGE_LIST_LEN;
+  dev->attrs.max_pkeys                  = FRDMA_MAX_PKEYS;
+  dev->attrs.local_ca_ack_delay         = FRDMA_LOCAL_CA_ACK_DELAY;
+}
+
+static void frdma_port_init( struct frdma_dev* dev ) {
+  dev->port.attr.state           = IB_PORT_DOWN;
+  dev->port.attr.max_mtu         = IB_MTU_4096;
+  dev->port.attr.active_mtu      = IB_MTU_256;
+  dev->port.attr.gid_tbl_len     = FRDMA_PORT_GID_TBL_LEN;
+  dev->port.attr.port_cap_flags  = FRDMA_PORT_PORT_CAP_FLAGS;
+  dev->port.attr.max_msg_sz      = FRDMA_PORT_MAX_MSG_SZ;
+  dev->port.attr.bad_pkey_cntr   = FRDMA_PORT_BAD_PKEY_CNTR;
+  dev->port.attr.qkey_viol_cntr  = FRDMA_PORT_QKEY_VIOL_CNTR;
+  dev->port.attr.pkey_tbl_len    = FRDMA_PORT_PKEY_TBL_LEN;
+  dev->port.attr.lid             = FRDMA_PORT_LID;
+  dev->port.attr.sm_lid          = FRDMA_PORT_SM_LID;
+  dev->port.attr.lmc             = FRDMA_PORT_LMC;
+  dev->port.attr.max_vl_num      = FRDMA_PORT_MAX_VL_NUM;
+  dev->port.attr.sm_sl           = FRDMA_PORT_SM_SL;
+  dev->port.attr.subnet_timeout  = FRDMA_PORT_SUBNET_TIMEOUT;
+  dev->port.attr.init_type_reply = FRDMA_PORT_INIT_TYPE_REPLY;
+  dev->port.attr.active_width    = FRDMA_PORT_ACTIVE_WIDTH;
+  dev->port.attr.active_speed    = FRDMA_PORT_ACTIVE_SPEED;
+  dev->port.attr.phys_state      = FRDMA_PORT_PHYS_STATE;
+}
 
 static __init int frdma_init_module( void ) {
   printk( KERN_WARNING "frdma init module\n" );
@@ -198,6 +207,8 @@ static __init int frdma_init_module( void ) {
   // }
   // dev->netdev = NULL;
 
+  frdma_attr_init( dev );
+  frdma_port_init( dev );
   ib_set_device_ops( &dev->ibdev, &frdma_device_ops );
 
   // ret = frdma_get_netdev(dev);
--- impl/fake_driver.h
+++ impl/fake_driver.h
@@ -1,6 +1,7 @@
 #ifndef __FAKE_DRIVER_H__
 #define __FAKE_DRIVER_H__
 
+#include "rdma/ib_mad.h"
 #include <linux/netdevice.h>
 #include <linux/pci.h>
 #include <linux/types.h>
@@ -11,20 +12,174 @@
 #define FRDMA_PCI_VENDOR_ID 0x6672// 0x66 0x72
 #define FRDMA_PCI_DEVICE_ID 0x646d// 0x64 0x6d 0x61
 
-#define FRDMA_FUNC_BAR 0
-#define FRDMA_MISX_BAR 2
-#define FRDMA_BAR_MASK ( BIT( FRDMA_FUNC_BAR ) | BIT( FRDMA_MISX_BAR ) )
+#define FRDMA_FUNC_BAR    0
+#define FRDMA_MISX_BAR    2
+#define FRDMA_BAR_MASK    ( BIT( FRDMA_FUNC_BAR ) | BIT( FRDMA_MISX_BAR ) )
+#define DEFAULT_MAX_VALUE ( 1 << 20 )
+
+enum rxe_device_param {
+  FRDMA_MAX_MR_SIZE      = -1ull,
+  FRDMA_PAGE_SIZE_CAP    = 0xfffff000,
+  FRDMA_MAX_QP_WR        = DEFAULT_MAX_VALUE,
+  FRDMA_DEVICE_CAP_FLAGS = IB_DEVICE_BAD_PKEY_CNTR | IB_DEVICE_BAD_QKEY_CNTR | IB_DEVICE_AUTO_PATH_MIG | IB_DEVICE_CHANGE_PHY_PORT | IB_DEVICE_UD_AV_PORT_ENFORCE | IB_DEVICE_PORT_ACTIVE_EVENT | IB_DEVICE_SYS_IMAGE_GUID | IB_DEVICE_RC_RNR_NAK_GEN | IB_DEVICE_SRQ_RESIZE | IB_DEVICE_MEM_MGT_EXTENSIONS | IB_DEVICE_MEM_WINDOW | IB_DEVICE_FLUSH_GLOBAL | IB_DEVICE_FLUSH_PERSISTENT
+#ifdef CONFIG_64BIT
+                           | IB_DEVICE_MEM_WINDOW_TYPE_2B | IB_DEVICE_ATOMIC_WRITE,
+#else
+                           | IB_DEVICE_MEM_WINDOW_TYPE_2B,
+#endif /* CONFIG_64BIT */
+  FRDMA_MAX_SGE                 = 32,
+  FRDMA_MAX_SGE_RD              = 32,
+  FRDMA_MAX_CQ                  = DEFAULT_MAX_VALUE,
+  FRDMA_MAX_LOG_CQE             = 15,
+  FRDMA_MAX_PD                  = DEFAULT_MAX_VALUE,
+  FRDMA_MAX_QP_RD_ATOM          = 128,
+  FRDMA_MAX_RES_RD_ATOM         = 0x3f000,
+  FRDMA_MAX_QP_INIT_RD_ATOM     = 128,
+  FRDMA_MAX_MCAST_GRP           = 8192,
+  FRDMA_MAX_MCAST_QP_ATTACH     = 56,
+  FRDMA_MAX_TOT_MCAST_QP_ATTACH = 0x70000,
+  FRDMA_MAX_AH                  = ( 1 << 15 ) - 1, /* 32Ki - 1 */
+  FRDMA_MIN_AH_INDEX            = 1,
+  FRDMA_MAX_AH_INDEX            = FRDMA_MAX_AH,
+  FRDMA_MAX_SRQ_WR              = DEFAULT_MAX_VALUE,
+  FRDMA_MIN_SRQ_WR              = 1,
+  FRDMA_MAX_SRQ_SGE             = 27,
+  FRDMA_MIN_SRQ_SGE             = 1,
+  FRDMA_MAX_FMR_PAGE_LIST_LEN   = 512,
+  FRDMA_MAX_PKEYS               = 64,
+  FRDMA_LOCAL_CA_ACK_DELAY      = 15,
+
+  FRDMA_MAX_UCONTEXT = DEFAULT_MAX_VALUE,
+
+  FRDMA_NUM_PORT = 1,
+
+  FRDMA_MIN_QP_INDEX = 16,
+  FRDMA_MAX_QP_INDEX = DEFAULT_MAX_VALUE,
+  FRDMA_MAX_QP       = DEFAULT_MAX_VALUE - FRDMA_MIN_QP_INDEX,
+
+  FRDMA_MIN_SRQ_INDEX = 0x00020001,
+  FRDMA_MAX_SRQ_INDEX = DEFAULT_MAX_VALUE,
+  FRDMA_MAX_SRQ       = DEFAULT_MAX_VALUE - FRDMA_MIN_SRQ_INDEX,
+
+  FRDMA_MIN_MR_INDEX = 0x00000001,
+  FRDMA_MAX_MR_INDEX = DEFAULT_MAX_VALUE >> 1,
+  FRDMA_MAX_MR       = FRDMA_MAX_MR_INDEX - FRDMA_MIN_MR_INDEX,
+  FRDMA_MIN_MW_INDEX = FRDMA_MAX_MR_INDEX + 1,
+  FRDMA_MAX_MW_INDEX = DEFAULT_MAX_VALUE,
+  FRDMA_MAX_MW       = FRDMA_MAX_MW_INDEX - FRDMA_MIN_MW_INDEX,
+
+  FRDMA_MAX_PKT_PER_ACK = 64,
+
+  FRDMA_MAX_UNACKED_PSNS = 128,
+
+  /* Max inflight SKBs per queue pair */
+  FRDMA_INFLIGHT_SKBS_PER_QP_HIGH = 64,
+  FRDMA_INFLIGHT_SKBS_PER_QP_LOW  = 16,
+
+  /* Max number of interations of each work item
+	 * before yielding the cpu to let other
+	 * work make progress
+	 */
+  FRDMA_MAX_ITERATIONS = 1024,
+
+  /* Delay before calling arbiter timer */
+  FRDMA_NSEC_ARB_TIMER_DELAY = 200,
+
+  /* IBTA v1.4 A3.3.1 VENDOR INFORMATION section */
+  FRDMA_VENDOR_ID = 0XFFFFFF,
+};
+
+
+enum frdma_port_param {
+  FRDMA_PORT_GID_TBL_LEN     = 1024,
+  FRDMA_PORT_PORT_CAP_FLAGS  = IB_PORT_CM_SUP,
+  FRDMA_PORT_MAX_MSG_SZ      = 0x800000,
+  FRDMA_PORT_BAD_PKEY_CNTR   = 0,
+  FRDMA_PORT_QKEY_VIOL_CNTR  = 0,
+  FRDMA_PORT_LID             = 0,
+  FRDMA_PORT_SM_LID          = 0,
+  FRDMA_PORT_SM_SL           = 0,
+  FRDMA_PORT_LMC             = 0,
+  FRDMA_PORT_MAX_VL_NUM      = 1,
+  FRDMA_PORT_SUBNET_TIMEOUT  = 0,
+  FRDMA_PORT_INIT_TYPE_REPLY = 0,
+  FRDMA_PORT_ACTIVE_WIDTH    = IB_WIDTH_1X,
+  FRDMA_PORT_ACTIVE_SPEED    = 1,
+  FRDMA_PORT_PKEY_TBL_LEN    = 1,
+  FRDMA_PORT_PHYS_STATE      = IB_PORT_PHYS_STATE_POLLING,
+  FRDMA_PORT_SUBNET_PREFIX   = 0xfe80000000000000ULL,
+};
+
+struct frdma_port {
+  struct ib_port_attr attr;
+};
 
 struct frdma_devattr {
   unsigned char peer_addr[ ETH_ALEN ];
   int           numa_node;
+
+  u64                fw_ver;
+  __be64             sys_image_guid;
+  u64                max_mr_size;
+  u64                page_size_cap;
+  u32                vendor_id;
+  u32                vendor_part_id;
+  u32                hw_ver;
+  int                max_qp;
+  int                max_qp_wr;
+  u64                device_cap_flags;
+  u64                kernel_cap_flags;
+  int                max_send_sge;
+  int                max_recv_sge;
+  int                max_sge_rd;
+  int                max_cq;
+  int                max_cqe;
+  int                max_mr;
+  int                max_pd;
+  int                max_qp_rd_atom;
+  int                max_ee_rd_atom;
+  int                max_res_rd_atom;
+  int                max_qp_init_rd_atom;
+  int                max_ee_init_rd_atom;
+  enum ib_atomic_cap atomic_cap;
+  enum ib_atomic_cap masked_atomic_cap;
+  int                max_ee;
+  int                max_rdd;
+  int                max_mw;
+  int                max_raw_ipv6_qp;
+  int                max_raw_ethy_qp;
+  int                max_mcast_grp;
+  int                max_mcast_qp_attach;
+  int                max_total_mcast_qp_attach;
+  int                max_ah;
+  int                max_srq;
+  int                max_srq_wr;
+  int                max_srq_sge;
+  unsigned int       max_fast_reg_page_list_len;
+  unsigned int       max_pi_fast_reg_page_list_len;
+  u16                max_pkeys;
+  u8                 local_ca_ack_delay;
+  int                sig_prot_cap;
+  int                sig_guard_cap;
+  struct ib_odp_caps odp_caps;
+  uint64_t           timestamp_mask;
+  uint64_t           hca_core_clock; /* in KHZ */
+  struct ib_rss_caps rss_caps;
+  u32                max_wq_type_rq;
+  u32                raw_packet_caps; /* Use ib_raw_packet_caps enum */
+  struct ib_tm_caps  tm_caps;
+  struct ib_cq_caps  cq_caps;
+  u64                max_dm_size;
+  /* Max entries for sgl for optimized performance per READ */
+  u32 max_sgl_rd;
 };
 
 struct frdma_dev {
   struct ib_device   ibdev;
   struct net_device* netdev;
 
-  struct frdma_devattr* attrs;
+  struct frdma_devattr attrs;
+  struct frdma_port    port;
 
   struct list_head cep_list;
 };
